81887c093ff5430d216c830e1d49c289c8f548bf
onichandame <zxinmyth@gmail.com>
2020-06-08 10:41:29 +0000

    testing

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { setFailed, info, getInput } from "@actions/core"
+import { connect } from "ts-nats"
+
+const parseServers = (): string[] =>
+  getInput("servers")
+    .split(" ")
+    .filter(v => !!v)
+
+const servers = parseServers()
+
+const testServer = async (server: string) =>
+  connect(server).catch(e => {
+    info(`testing server ${server}`)
+    setFailed(`server ${server} failed due to ${JSON.stringify(e)}`)
+  })
+;(async () => {
+  const con: Promise<any>[] = []
+  for (let server of servers) con.push(testServer(server))
+  await Promise.all(con)
+})()
