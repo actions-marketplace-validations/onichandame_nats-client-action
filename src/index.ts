03031e3b6ee6b68abb71216e55fe855cae290a48
onichandame <zxinmyth@gmail.com>
2020-06-08 16:04:25 +0000

    testing

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
 import { setFailed, info, getInput } from "@actions/core"
-import { generate } from "randomstring"
 import { connect } from "ts-nats"
-
-const randomOptions: Parameters<typeof generate>[0] = {
-  length: 20,
-  charset: "alphanumeric"
-}
 
 const parseServers = (): string[] =>
   getInput("servers")
@@ -26,26 +20,6 @@ const testServer = async (server: string) => {
     if (getInput("cluster") === "true") {
       info("testing cluster")
       const p: Promise<any>[] = []
-      for (let server of servers) {
-        info(`testing subscription on ${server}`)
-        const subject = generate(randomOptions)
-        const nc = await connect(server)
-        p.push(
-          new Promise((r, j) => {
-            let count = 0
-            nc.subscribe(subject, () => {
-              info(`${server} ${count + 1}/${servers.length}`)
-              if (++count === servers.length) r()
-            })
-            setTimeout(() => j(new Error(`subscription timeout`)), 5000)
-          })
-        )
-        for (let target of servers)
-          connect(target).then(nc => {
-            nc.publish(subject)
-            p.push(Promise.resolve(nc.flush()))
-          })
-      }
       await Promise.all(p)
     }
   } catch (e) {
